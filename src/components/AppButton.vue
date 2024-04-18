c0a897ba26c61de36b176447434950f90fe23d30
habibirrahman <habibirrahman18@gmail.com>
2024-04-18 15:51:10 +0700

    feat: button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+export interface Props {
+  disabled?: boolean
+  type?: 'button' | 'submit'
+}
+export interface Emits {
+  (e: 'click'): void
+}
+
+withDefaults(defineProps<Props>(), {
+  disabled: false,
+  type: 'button'
+})
+defineEmits<Emits>()
+</script>
+
+<template>
+  <button
+    class="ring-light-purple-200 bg-light-purple hover:bg-dark-purple-200 relative flex items-center justify-center rounded px-4 py-2 text-white ring-offset-2 active:ring-2"
+    :disabled="disabled"
+    @click="$emit('click')"
+    :type="type"
+  >
+    <slot />
+  </button>
+</template>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,8 +1,14 @@
+<script setup lang="ts">
+import AppButton from '@/components/AppButton.vue'
+function onClick(text: string) {
+  alert(text)
+}
+</script>
+
 <template>
-  <div>
-    
+  <div class="flex flex-col items-center gap-4 py-4">
+    <AppButton @click="onClick('alert')">Alert</AppButton>
   </div>
 </template>
 
-<style>
-</style>
+<style></style>
