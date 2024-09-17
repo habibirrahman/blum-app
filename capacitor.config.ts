55ffe8635b7e6b626932183e49fe1a8ddababa2e
habibirrahman <habibirrahman18@gmail.com>
2024-09-17 15:59:47 +0700

    feat: update config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
-import type { CapacitorConfig } from '@capacitor/cli';
+import type { CapacitorConfig } from '@capacitor/cli'
 
 const config: CapacitorConfig = {
   appId: 'tech.simpul.blum',
   appName: 'Blum',
-  webDir: 'dist'
-};
+  webDir: 'dist',
+  plugins: {
+    CapacitorHttp: {
+      enabled: true
+    }
+  }
+}
 
-export default config;
+export default config
